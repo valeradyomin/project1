858f5f558539561a09f826da5b942324382fc304
Valera Demin <wahrwolffe@gmail.com>
2023-12-08 03:03:05 +0300

    7. Реализуем счетчик просмотров

--- materials/models.py
+++ materials/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+NULLABLE = {"null": True, "blank": True}
+
 
 class Material(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     body = models.TextField(verbose_name='содержимое')
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
 
     def __str__(self):
         return self.title
--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from materials.models import Material
 
@@ -12,19 +13,49 @@ class MaterialCreateView(CreateView):
     fields = ('title', 'body',)
     success_url = reverse_lazy('materials:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_material = form.save()
+            new_material.slug = slugify(new_material.title)
+            new_material.save()
+
+        return super().form_valid(form)
+
 
 class MaterialListView(ListView):
     model = Material
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)  # *args, **kwargs - optional
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class MaterialDetailView(DetailView):
     model = Material
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class MaterialUpdateView(UpdateView):
     model = Material
     fields = ('title', 'body',)
-    success_url = reverse_lazy('materials:list')
+    # success_url = reverse_lazy('materials:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_material = form.save()
+            new_material.slug = slugify(new_material.title)
+            new_material.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('materials:view',  args=[self.kwargs.get('pk')])
 
 
 class MaterialDeleteView(DeleteView):
