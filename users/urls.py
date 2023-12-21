e250d30f7e9ad5f3c8737d2f99cf44f06c9f50ea
Valera <wahrwolffe@gmail.com>
2023-12-22 02:56:23 +0300

    register

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     # existed default controller
     path('logout/', LogoutView.as_view(), name='logout'),
-    # controller need create manually
+    # controller need create manually from generics
     path('register/', RegisterView.as_view(), name='register'),
 ]
