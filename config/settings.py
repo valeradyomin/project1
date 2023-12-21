494f3bcad966e06054b3fd2e60cbfe928056ef14
Valera <wahrwolffe@gmail.com>
2023-12-22 00:32:07 +0300

    settings set

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'main',
     'materials',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
