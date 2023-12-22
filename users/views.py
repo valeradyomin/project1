1bbfe4206b3a9399f274eddd1cc74d4be90a8940
Valera <wahrwolffe@gmail.com>
2023-12-22 03:14:36 +0300

    RegisterView

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from users.forms import UserRegisterForm
 from users.models import User
 
 
@@ -9,5 +11,7 @@ from users.models import User
 
 class RegisterView(CreateView):
     model = User
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     template_name = 'users/register.html'
+    #TODO (tc: 21-48
+    success_url = reverse_lazy('users:login')
