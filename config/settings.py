f3793e581c6509aed91b7d72f6a982cd2c25a0c6
Valera Demin <wahrwolffe@gmail.com>
2023-12-28 21:35:53 +0300

    23.2 Кеширование и работа с переменными окружения

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
